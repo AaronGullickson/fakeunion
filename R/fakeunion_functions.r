7c282d8ac027f65e0d6c48b2ec93628f22a2171b
Aaron Gullickson <aarong@uoregon.edu>
2017-04-21 11:52:46 -0700

    added poolChoiceModel and createExogamyTerms to package

--- R/fakeunion_functions.r
+++ R/fakeunion_functions.r
@@ -44,29 +44,28 @@
 #' @examples
 #' #generate three counterfactual couples for each real couples
 #' #in example ACS data
-#' markets <- generateCouples(3,acs.couples,
-#'                            acs.malealters,acs.femalealters,
-#'                            "state",weight="perwt",keep="hhwt")
+#' market <- generateCouples(3,acs.couples,
+#'                           acs.malealters,acs.femalealters,
+#'                           "state",weight="perwt",keep="hhwt")
 #'
 #' #check that there is one real marriage and three counterfactual
 #' #marriages for each case
-#' summary(tapply(markets$choice,markets$group,sum))
-#' summary(tapply(!markets$choice,markets$group,sum))
+#' summary(tapply(market$choice,market$group,sum))
+#' summary(tapply(!market$choice,market$group,sum))
 #'
 #' \dontrun{
 #' #load survival function and run clogit command to estimate how age
 #' #differences and racial exogamy affect the log-odds of union formation
 #' require(survival)
 #' model <- clogit(choice~I(ageh-agew)+I((ageh-agew)^2)+I(raceh!=racew)
-#'                        +strata(group), data=markets)
+#'                        +strata(group), data=market)
 #' summary(model)
 #' }
 generateCouples <- function(n, actual, men, women, geo, id="id",
                             weight=NULL, keep=NULL, verbose=TRUE) {
 
   if(!require(wrswoR)) {
-    cat("package wrswoR not found")
-    return()
+    stop("package wrswoR must be installed.")
   }
 
   if(verbose) {
@@ -247,4 +246,133 @@ organizeColumns <- function(couples, geo, keep=NULL) {
   return(couples[c(which(colnames(couples) %in% c(geo,"group","choice",keep)),
                    grep("h$",colnames(couples)),
                    grep("w$",colnames(couples)))])
+}
+
+
+
+
+#' Run fixed effects conditional logit model on multiple datasets and pool results
+#'
+#' @description Given a list of multiple datasets of real and counterfactual unionts and a model formula, this function
+#'              will calculate a discrete choice model for each dataset and pool the results, taking account of both
+#'              within and between variance in calculating the standard error.
+#' @param formula an object of class \code{\link[stats]{formula}} specifying the \code{\link[survival]{clogit}} model to be
+#'                performed on each dataset.
+#' @param datasets a list of datasets where each dataset is produced from the \code{\link{generateCouples}} function.
+#' @details Because the dataset of real and counterfactual unions is created by sampling among all the possible alternate partners,
+#'          the results of models will vary as a result of this sampling process. Therefore, it may be useful to generate multiple
+#'          datasets and pool model results across these datasets in a manner identical to multiple imputation, where the standard
+#'          errors of estimates are adjusted for the variance in coefficient estimates across datasets.
+#'
+#'          This function is a convenience function that will perform this pooling and produce properly adjusted results.
+#'          The reported coefficients from the model are given by taking the mean across all datasets. The reported variance \eqn{V}
+#'          for each parameter is given by:
+#'          \deqn{V=W+(1+1/m)B}
+#'          Where \eqn{m} is the number of datasets, \eqn{W} is the within variance, estimated by the square of the mean standard
+#'          error across datasets, and \eqn{B} is the between variance estimated by the variance of coefficient estimates across
+#'          datasets.
+#'
+#'          Models are estimated using the \code{\link[survival]{clogit}} function from the \pkg{survival} package. This package
+#'          must be installed.
+#' @return a data.frame object with the following elements:
+#' \item{b.pool}{The average coefficient across datasets.}
+#' \item{se.pool}{standard error that combined within and between variance}
+#' \item{z.pool}{z-statistic from dividing b by se}
+#' \item{pvalue.pool}{p-value for the hypothesis test that the coefficient is zero in the population}
+#' \item{within.var}{The square of the mean standard error across datasets}
+#' \item{between.var}{the variance of the coefficient across datasets}
+#' @examples
+#' markets <- replicate(5, generateCouples(3,acs.couples,
+#'                            acs.malealters,acs.femalealters,
+#'                            "state",weight="perwt",verbose=FALSE),
+#'                      simplify=FALSE)
+#'
+#' poolChoiceModel(choice~ageh+I(ageh^2)+I(ageh-agew)+I((ageh-agew)^2)+strata(group),
+#'                 markets)
+poolChoiceModel <- function(formula, datasets) {
+  if(!require(survival)) {
+    stop("survival package must be installed.")
+  }
+  models <- lapply(datasets, function(dataset) {clogit(formula, data=dataset)})
+
+  m <- length(models)
+  b <- sapply(models, coef)
+  se <- sapply(models, function(model) {summary(model)$coef[,3]})
+
+  b.pool <- apply(b,1,mean)
+  between.var <- apply(b,1,var)
+  within.var <- apply(se^2,1,mean)
+  se.pool <- sqrt(within.var+between.var+between.var/m)
+  z.pool <- b.pool/se.pool
+  pvalue.pool <- (1-pnorm(abs(z.pool)))*2
+
+  return(data.frame(b.pool,se.pool,z.pool,pvalue.pool,within.var,between.var))
+}
+
+
+#this will take a husband and wife categorical variable and generate a set of
+#exogamy categories, where endogamy is the reference. It can also make these
+#terms symmetric
+
+#' Create exogamy terms from partner's characteristics
+#'
+#' @description This function will calculate a variable that combines two identical factor
+#' variables for each partner, replacing all cases where partners match to a reference category
+#' of endogamous unions.
+#' @param varh A factor vector giving the categories for the "husband."
+#' @param varw A factor vector giving the categories for the "wife."
+#' @param symmetric If set to true, then the ordering of the categories by partner will be ignored.
+#' @details This function operates much like the \code{\link[base]{interaction}} function to combine partner
+#'  characteristics. The function assumes that the two variables being combined are identical categorical variables
+#'  for each partner (e.g. husband's and wife's education, husband's and wife's race). It differs from a basic interaction
+#'  in two ways. First, any cases where husband and wife have the same category will be recorded as the "Endog" category and
+#'  will be set to the reference. Second, users can specify that terms should be symmetric such that couples are put in the
+#'  same exogamous category without respect to whether it was the husband or wife who belonged to the specific categories.
+#'  For example, if symmetric, then the category for white husbands and black wives would be the same as for black husbands
+#'  and white wives.
+#'
+#'
+#' @return a factor vector containing the combined categories, with "Endog" set as the reference.
+#' @examples
+#'  market <- generateCouples(3,acs.couples,
+#'                           acs.malealters,acs.femalealters,
+#'                           "state",weight="perwt")
+#'  market$rexog <- createExogamyTerms(market$raceh, market$racew)
+#'  market$rexog.sym <- createExogamyTerms(market$raceh, market$racew, symmetric=TRUE)
+#'  levels(market$rexog)
+#'  levels(market$rexog.sym)
+#'
+#'  \dontrun{
+#'  require(survival)
+#'  clogit(choice~rexog+strata(group), data=market)
+#'  clogit(choice~rexog.sym+strata(group), data=market)
+#'  }
+createExogamyTerms <- function(varh, varw, symmetric=FALSE) {
+  ncat <- nlevels(varh)
+  if(nlevels(varw) != ncat) {
+    stop("partner variables have different number of categories")
+  }
+  if(any(!(levels(varw) %in% levels(varh)))) {
+    stop("partner variables have different sets of categories")
+  }
+  exog <- interaction(varh, varw, sep=".", lex.order=TRUE)
+  #replace all couples with same characterstics with a single endogamous ("Endog") level
+  l <- levels(exog)
+  l[seq(from=1, by=(ncat+1),to=length(l))] <- "Endog"
+  if(symmetric) {
+    #ignore the ordering of husbands and wives
+    for(i in 1:length(l)) {
+      if(l[i]=="Endog") {
+        next
+      }
+      x <- strsplit(l[i],"\\.")
+      reverse <- paste(x[[1]][2],x[[1]][1],sep=".")
+      if(which(l==reverse)<i) {
+        l[i] <- reverse
+      }
+    }
+  }
+  levels(exog) <- l
+  exog <- relevel(exog, ref="Endog")
+  return(exog)
 }
